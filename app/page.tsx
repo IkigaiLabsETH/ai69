3bd3469d47f5145c3b1fa5e9a64175be04dba318
ikigai <ikigailabs@proton.me>
2023-09-01 17:37:20 +0200

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,13 @@ export default function Home() {
 
   return (
     <div>
-    <ChatWindow
-      endpoint="api/chat"
-      emoji="🐻⛓️"
-      titleText="GM GM Anon"
-      placeholder="I'm a fricking degen tbh! Ask me about berachain!"
-    />
+      <ChatWindow
+        endpoint="api/chat"
+        emoji="🐻⛓️"
+        titleText="GM GM Anon"
+        placeholder="I'm a fricking degen tbh! Ask me about berachain!"
+        emptyStateComponent={<div>No messages to display.</div>}
+      />
   </div>
   );
 }
