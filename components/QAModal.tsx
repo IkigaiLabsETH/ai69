471ba83929e22e108c7da64f759e123cb17deaa4
ikigai <ikigailabs@proton.me>
2023-09-02 23:34:49 +0200

    update

--- components/QAModal.tsx
+++ components/QAModal.tsx
@@ -3,11 +3,9 @@
 import {Fragment, useEffect, useState} from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { useCompletion } from "ai/react";
-import {ChatBlock, responseToChatBlocks} from "@/components/compagnions/ChatBlock";
+
 
 var last_name = "";
-
-
 
 export default function QAModal({
   open,
@@ -39,19 +37,7 @@ export default function QAModal({
     headers: { name: example.name },
   });
 
-  let [blocks, setBlocks] = useState<typeof ChatBlock[] | null>(null);
-  useEffect(() => {
-    // When the completion changes, parse it to multimodal blocks for display.
-    if (completion) {
-      setBlocks(responseToChatBlocks(completion).map((element) => () => element));    } else {
-      setBlocks(null)
-    }
-  }, [completion])
 
-  if (!example) {
-    console.log("ERROR: no companion selected");
-    return null;
-  }
 
   const handleClose = () => {
     setInput("");
@@ -94,7 +80,7 @@ export default function QAModal({
                       className={"w-full flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 shadow-sm focus:outline-none sm:text-sm sm:leading-6 " + (isLoading && !completion ? "text-gray-600 cursor-not-allowed" : "text-white")}                      
                       value={input}
                       onChange={handleInputChange}
-                      disabled={isLoading && !blocks}
+                     
                     />
                   </form>
                   <div className="mt-3 sm:mt-5">
@@ -103,36 +89,9 @@ export default function QAModal({
                         Chat with {example.name}
                       </p>
                     </div>
-                    {blocks && (
-                      <div className="mt-2">
-                        {blocks}
-                      </div>
-                    )}
+                    
 
-                    {isLoading && !blocks && (
-                      <p className="flex items-center justify-center mt-4">
-                        <svg
-                          className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                        >
-                          <circle
-                            className="opacity-25"
-                            cx="12"
-                            cy="12"
-                            r="10"
-                            stroke="currentColor"
-                            stroke-width="4"
-                          ></circle>
-                          <path
-                            className="opacity-75"
-                            fill="currentColor"
-                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                          ></path>
-                        </svg>
-                      </p>
-                    )}
+                    
                   </div>
                 </div>
               </Dialog.Panel>
